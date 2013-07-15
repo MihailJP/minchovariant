0e170cd7072bd5c5b1068c95ff587145cdcd46c3
TANIGAWA Takashi <mihailjp@gmail.com>
2013-07-15 14:53:57 +0900

    JISX0208 Level 1 codepoint list

--- makettf-patch.sed
+++ makettf-patch.sed
@@ -1,3 +1,10 @@
 /^Move(400, -400)/d
 /^Move(0, 50)/d
 /^SetWidth(1000)/i Scale(20)
+/^foreach(sort(keys %buhin)){/,/^}/c open GLYPHLIST, "../jisx0208-level-1.txt" or die "Cannot read the glyph list";\
+while (<GLYPHLIST>) {\
+	chomp; my $name = $_;\
+	(my $target = $name) =~ s/^[uU]0*//g; # delete zero for the beginning\
+	$target{$target} = $name;\
+}\
+close GLYPHLIST;
--- mkparts.pl
+++ mkparts.pl
@@ -2,14 +2,6 @@
 
 while (<>) {
 	my $currLine = $_;
-	my $rplFlag = ((my $rplLine = $currLine) =~ s/^ ([uU][0-9a-fA-F]{4,}\S*) *\| \S+ *\| (\S+) *$/$1\t$2/);
-	my $glyphName = $1;
-	if ($rplFlag) {
-		if ($glyphName eq "u6c38") {
-			print $rplLine;
-		}
-	} else {
-		$rplFlag = (($rplLine = $currLine) =~ s/^ (\S+) *\| \S+ *\| (\S+) *$/$1\t$2/);
-		print $rplLine if ($rplFlag);
-	}
+	$rplFlag = (($rplLine = $currLine) =~ s/^ (\S+) *\| \S+ *\| (\S+) *$/$1\t$2/);
+	print $rplLine if ($rplFlag);
 }
