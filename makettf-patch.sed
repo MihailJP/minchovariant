647217c12897712b9fff0213e231eb6dc24694aa
TANIGAWA Takashi <mihailjp@gmail.com>
2013-07-16 01:06:01 +0900

    Fix missing contours

--- makettf-patch.sed
+++ makettf-patch.sed
@@ -1,6 +1,12 @@
 /^Move(400, -400)/d
 /^Move(0, 50)/d
+/^RemoveOverlap()/d
+/^Simplify()/d
 /^SetWidth(1000)/i Scale(20)
+/^Scale(500)/a CanonicalContours()\
+CanonicalStart()\
+FindIntersections()
+s/"Generate(\(.*\)\.ttf.*)/"Save(\1.sfd\\")/
 /^foreach(sort(keys %buhin)){/,/^}/c open GLYPHLIST, "../jisx0208-level-1.txt" or die "Cannot read the glyph list";\
 while (<GLYPHLIST>) {\
 	chomp; my $name = $_;\
--- mkparts.pl
+++ mkparts.pl
@@ -1,7 +1,18 @@
 #!/usr/bin/perl
 
 while (<>) {
+	chomp;
 	my $currLine = $_;
 	$rplFlag = (($rplLine = $currLine) =~ s/^ (\S+) *\| \S+ *\| (\S+) *$/$1\t$2/);
-	print $rplLine if ($rplFlag);
+	$rplLine =~ s/\@\d+//g;
+	my @rplLine = split(/\t/, $rplLine);
+	my @rplRecord = split(/\$/, $rplLine[1]);
+	my @rplRecordNew;
+	foreach (@rplRecord) {
+		my @rplField = split(/:/, $_);
+		push (@rplRecordNew, join(':', ($rplField[0] == 99) ? @rplField[0..7] : @rplField));
+	}
+	$rplLine[1] = join('$', @rplRecordNew);
+	$rplLine = join("\t", @rplLine);
+	print $rplLine, "\n" if ($rplFlag);
 }
