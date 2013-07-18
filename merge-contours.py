b259a660ce51b5f8dab661cedbd0923c08043439
TANIGAWA Takashi <mihailjp@gmail.com>
2013-07-18 20:56:57 +0900

    Workaround for broken contours

--- merge-contours.py
+++ merge-contours.py
@@ -1,15 +1,57 @@
 #!/usr/bin/env fontforge
 
+def separate(contour):
+	layer = fontforge.layer()
+	for i in range(0, len(contour) - 1):
+		for j in range(len(contour) - 1, i, -1):
+			if contour[i] == contour[j] and contour[i].on_curve and contour[j].on_curve:
+				c1 = fontforge.contour()
+				c2 = fontforge.contour()
+				c1 += contour[0:i]
+				c1 += contour[j:len(contour)]
+				c2 += contour[i:j]
+				c1.closed = c2.closed = True
+				try:
+					if c1.isClockwise() == 0:
+						c1.reverseDirection()
+				except AttributeError:
+					pass
+				try:
+					if c2.isClockwise() == 0:
+						c2.reverseDirection()
+				except AttributeError:
+					pass
+				layer += c1
+				return layer + separate(c2)
+	layer += contour
+	return layer
+
+def separateSelfIntersect(layer):
+	l = fontforge.layer()
+	for contour in layer:
+		if contour.selfIntersects():
+			l += separate(contour)
+		elif contour.isClockwise() == 0:
+			l += contour.reverseDirection()
+		else:
+			l += contour
+	return l
+
 import fontforge
 font = fontforge.open("work.sfd")
 for glyph in font.glyphs():
 	if glyph.isWorthOutputting():
-		layer = glyph.layers[1]
+		layer = separateSelfIntersect(glyph.layers[1])
 		if not layer.isEmpty():
 			newLayer = fontforge.layer()
 			newLayer += layer[0]
-			for i in range(1, len(layer)):
-				newLayer += layer[i]
-				newLayer.removeOverlap()
+			try:
+				for i in range(1, len(layer)):
+					newLayer += layer[i]
+					newLayer.removeOverlap()
+			except Exception, ex:
+				print glyph.glyphname, ex
+				if ex.args[0] != "Empty contour":
+					raise
 			glyph.layers[1] = newLayer
 font.generate("work.ttf")
