baea0ea77be722b8343a3ed4b93ba27b4ae2f6ee
TANIGAWA Takashi <mihailjp@gmail.com>
2013-12-24 22:27:05 +0900

    Make feature 'zero' be generated

--- genfeat.rb
+++ genfeat.rb
@@ -7,16 +7,26 @@ GlyphDat = YAML.load(open('glyphmap.yml') {|f| f.read})
 
 print(open('feathead.txt') {|f| f.read})
 
-FeatureDat.each {|featName, featDat|
+FeatureDat.each {|featName, featStyle|
 	print("feature #{featName} {\n")
-	featDat.each {|fName, field|
-		GlyphDat.each {|gName, gDat|
-			field.each {|fromTag|
-				print("\tsub \\#{gDat[fromTag]} by \\#{gDat[fName]};\n") if gDat[fromTag] && gDat[fName]
+	featStyle.each {|featStyleName, featDat|
+		if featStyleName == 'font-to-font' then
+			featDat.each {|fName, field|
+				GlyphDat.each {|gName, gDat|
+					field.each {|fromTag|
+						print("\tsub \\#{gDat[fromTag]} by \\#{gDat[fName]};\n") if gDat[fromTag] && gDat[fName]
+					}
+				}
 			}
-		}
-		print("} #{featName};\n")
+		elsif featStyleName == 'glyph-to-glyph' then
+			featDat.each {|glyph|
+				GlyphDat[glyph['from']].each {|gName, gDat|
+					print("\tsub \\#{GlyphDat[glyph['from']][gName]} by \\#{GlyphDat[glyph['to']][gName]};\n") if GlyphDat[glyph['from']][gName] && GlyphDat[glyph['to']][gName]
+				}
+			}
+		end
 	}
+	print("} #{featName};\n")
 }
 
 print(open('featfoot.txt') {|f| f.read})
