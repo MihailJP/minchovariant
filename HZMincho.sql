d69aaeec3d6f5a28cb07afda1508de6301dc6ae1
TANIGAWA Takashi <mihailjp@gmail.com>
2014-01-13 19:24:25 +0900

    Update 'vert' and 'vrt2'

--- HZMincho.sql
+++ HZMincho.sql
@@ -1889,6 +1889,7 @@ UNION SELECT zeroNoSlash.rotItal AS fromCID, zeroWithSlash.rotItal AS toCID FROM
 CREATE VIEW oneToOneFeat AS
 SELECT 10 AS feat, horizontalFull AS fromCID, verticalFull AS toCID FROM kana WHERE horizontalFull IS NOT NULL AND verticalFull IS NOT NULL
 UNION SELECT 10 AS feat, horizontalRuby AS fromCID, verticalRuby AS toCID FROM kana WHERE horizontalRuby IS NOT NULL AND verticalRuby IS NOT NULL
+UNION SELECT 10 AS feat, horizontalTune AS fromCID, verticalTune AS toCID FROM kana WHERE horizontalTune IS NOT NULL AND verticalTune IS NOT NULL
 UNION SELECT 10 AS feat, horizontal AS fromCID, vertical AS toCID FROM cjkKumimoji WHERE horizontal IS NOT NULL AND vertical IS NOT NULL
 UNION SELECT 11 AS feat, pwid AS fromCID, rotPwid AS toCID FROM lgcGlyphs WHERE pwid IS NOT NULL AND rotPwid IS NOT NULL
 UNION SELECT 11 AS feat, hwid AS fromCID, rotHwid AS toCID FROM lgcGlyphs WHERE hwid IS NOT NULL AND rotHwid IS NOT NULL
@@ -1898,6 +1899,7 @@ UNION SELECT 11 AS feat, ital AS fromCID, rotItal AS toCID FROM lgcGlyphs WHERE 
 UNION SELECT 11 AS feat, horizontalFull AS fromCID, verticalFull AS toCID FROM kana WHERE horizontalFull IS NOT NULL AND verticalFull IS NOT NULL
 UNION SELECT 11 AS feat, horizontalHalf AS fromCID, verticalHalf AS toCID FROM kana WHERE horizontalHalf IS NOT NULL AND verticalHalf IS NOT NULL
 UNION SELECT 11 AS feat, horizontalRuby AS fromCID, verticalRuby AS toCID FROM kana WHERE horizontalRuby IS NOT NULL AND verticalRuby IS NOT NULL
+UNION SELECT 11 AS feat, horizontalTune AS fromCID, verticalTune AS toCID FROM kana WHERE horizontalTune IS NOT NULL AND verticalTune IS NOT NULL
 UNION SELECT 11 AS feat, horizontal AS fromCID, vertical AS toCID FROM cjkKumimoji WHERE horizontal IS NOT NULL AND vertical IS NOT NULL
 UNION SELECT 12 AS feat, horizontalFull AS fromCID, horizontalTune AS toCID FROM kana WHERE horizontalFull IS NOT NULL AND horizontalTune IS NOT NULL
 UNION SELECT 13 AS feat, horizontalFull AS fromCID, verticalTune AS toCID FROM kana WHERE horizontalFull IS NOT NULL AND verticalTune IS NOT NULL
