a38a16d4f198495ed50b0d8a147fed2f59f7ae69
TANIGAWA Takashi <mihailjp@gmail.com>
2014-10-19 02:03:50 +0900

    Modify KAGI

--- kagecd-patch.pl
+++ kagecd-patch.pl
@@ -2,6 +2,8 @@ my $urokoOcc = 0;
 my $urokoL = 0;
 my $a14Occ = 0;
 my $a14L = 0;
+my $hosomiOcc = 0;
+my $a22Flag = 0;
 my $cr = '';
 while (<>) {
 	$cr = "\r" if (/\r$/);
@@ -30,6 +32,24 @@ while (<>) {
 		print "      poly.push(x2 - kage.kWidth * 4 * Math.min(1 - opt2 / 10, Math.pow(kMinWidthT / kage.kMinWidthT, 3)) * jumpFactor, y2 - kMinWidthT);$cr";
 	} elsif ($a14Occ == 1 and $a14L == 5) {
 		print "      poly.push(x2 - kage.kWidth * 4 * Math.min(1 - opt2 / 10, Math.pow(kMinWidthT / kage.kMinWidthT, 3)) * jumpFactor, y2 - kMinWidthT * 0.5);$cr";
+	} elsif (/hosomi = 0.5;/ and $hosomiOcc == 0) {
+		++$hosomiOcc;
+		print "    var cornerOffset = 0;$cr";
+		print "    if(a1 == 22 && a2 == 7){$cr";
+		print "        cornerOffset = (kMinWidthT > 6) ? (kMinWidthT - 6) / 1.5 : 0;$cr";
+		print "        x1 += cornerOffset;$cr";
+		print "    }$cr";
+		print "    $cr";
+		print;
+	} elsif (/if\(a1 == 22\)\{ \/\/box's up-right corner/ and $a22Flag == 0) {
+		$a22Flag = 1;
+		print;
+	} elsif (/\}/ and $a22Flag == 1) {
+		$a22Flag = 2;
+		print;
+	} elsif ($a22Flag == 1) {
+		s/x1/x1 - cornerOffset/;
+		print;
 	} else {
 		print;
 	}
