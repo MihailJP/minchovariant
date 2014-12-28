1f2fd341b5902426397697d41590af9017e20060
TANIGAWA Takashi <mihailjp@gmail.com>
2014-12-28 13:25:25 +0900

    Add methods to Kage::Stroke

--- Kage.rb
+++ Kage.rb
@@ -48,6 +48,195 @@ module Kage
 				return nil
 			end
 		end
+		def strokeType
+			@data[0]
+		end
+		def startType
+			@data[1]
+		end
+		def endType
+			@data[2]
+		end
+		def strokeType=(val)
+			raise TypeError, "Not a Fixnum instance" unless val.is_a? Fixnum
+			origType = @data[0]
+			case origType
+			when 1
+				case val
+				when 1
+				when 2, 3, 4
+					@data.insert(5, (@data[3] + @data[5]) / 2, (@data[4] + @data[6]) / 2)
+				when 6, 7
+					@data.insert(5, (@data[3] * 2 + @data[5]) / 3, (@data[4] * 2 + @data[6]) / 3, (@data[3] + @data[5] * 2) / 3, (@data[4] + @data[6] * 2) / 3)
+				else
+					raise ArgumentError, "Stroke type is invalid"
+				end
+			when 2, 3, 4
+				case val
+				when 1
+					@data[5..8] = @data[7..8]
+				when 2, 3, 4
+				when 6, 7
+					@data[5..6] = [(@data[5] * 2 + @data[7]) / 3, (@data[6] * 2 + @data[8]) / 3, (@data[5] + @data[7] * 2) / 3, (@data[6] + @data[8] * 2) / 3]
+				else
+					raise ArgumentError, "Stroke type is invalid"
+				end
+			when 6, 7
+				case val
+				when 1
+					@data[5..10] = @data[9..10]
+				when 2, 3, 4
+					@data[5..8] = [(@data[5] + @data[7]) / 2, (@data[6] + @data[8]) / 2]
+				when 6, 7
+				else
+					raise ArgumentError, "Stroke type is invalid"
+				end
+			else
+				raise ArgumentError, "Stroke type is invalid"
+			end
+			@data[0] = val
+		end
+		def startType=(val)
+			raise TypeError, "Not a Fixnum instance" unless val.is_a? Fixnum
+			@data[1] = val
+		end
+		def endType=(val)
+			raise TypeError, "Not a Fixnum instance" unless val.is_a? Fixnum
+			@data[2] = val
+		end
+		def startPoint
+			case @data[0]
+			when 1, 2, 3, 4, 6, 7
+				return @data[3..4]
+			else
+				return nil
+			end
+		end
+		def controlPoint1
+			case @data[0]
+			when 2, 3, 4, 6, 7
+				return @data[5..6]
+			else
+				return nil
+			end
+		end
+		def controlPoint2
+			case @data[0]
+			when 2, 3, 4
+				return @data[5..6]
+			when 6, 7
+				return @data[7..8]
+			else
+				return nil
+			end
+		end
+		def controlPoint
+			case @data[0]
+			when 2, 3, 4
+				return @data[5..6]
+			when 6, 7
+				return [@data[5..6], @data[7..8]]
+			else
+				return nil
+			end
+		end
+		def endPoint
+			case @data[0]
+			when 1
+				return @data[5..6]
+			when 2, 3, 4
+				return @data[7..8]
+			when 6, 7
+				return @data[9..10]
+			else
+				return nil
+			end
+		end
+		def startPoint=(a)
+			raise TypeError, "Not an Array of 2 Fixnum instances" unless a.is_a? Array and a.length == 2 and a.all?{|elem| elem.is_a? Fixnum}
+			@data[3..4] = a
+		end
+		def controlPoint1=(a)
+			raise TypeError, "Not an Array of 2 Fixnum instances" unless a.is_a? Array and a.length == 2 and a.all?{|elem| elem.is_a? Fixnum}
+			case @data[0]
+			when 2, 3, 4, 6, 7
+				@data[5..6] = a
+			else
+				raise RuntimeError, "Stroke type is invalid"
+			end
+		end
+		def controlPoint2=(a)
+			raise TypeError, "Not an Array of 2 Fixnum instances" unless a.is_a? Array and a.length == 2 and a.all?{|elem| elem.is_a? Fixnum}
+			case @data[0]
+			when 2, 3, 4
+				@data[5..6] = a
+			when 6, 7
+				@data[7..8] = a
+			else
+				raise RuntimeError, "Stroke type is invalid"
+			end
+		end
+		def endPoint=(a)
+			raise TypeError, "Not an Array of 2 Fixnum instances" unless a.is_a? Array and a.length == 2 and a.all?{|elem| elem.is_a? Fixnum}
+			case @data[0]
+			when 1, 99
+				@data[5..6] = a
+			when 2, 3, 4
+				@data[7..8] = a
+			when 6, 7
+				@data[9..10] = a
+			else
+				raise RuntimeError, "Stroke type is invalid"
+			end
+		end
+		def startX
+			self.startPoint[0]
+		end
+		def startY
+			self.startPoint[1]
+		end
+		def control1X
+			self.controlPoint1[0]
+		end
+		def control1Y
+			self.controlPoint1[1]
+		end
+		def control2X
+			self.controlPoint2[0]
+		end
+		def control2Y
+			self.controlPoint2[1]
+		end
+		def endX
+			self.endPoint[0]
+		end
+		def endY
+			self.endPoint[1]
+		end
+		def startX=(val)
+			self.startPoint = [val, self.startY]
+		end
+		def startY=(val)
+			self.startPoint = [self.startX, val]
+		end
+		def control1X=(val)
+			self.controlPoint1 = [val, self.control1Y]
+		end
+		def control1Y=(val)
+			self.controlPoint1 = [self.control1X, val]
+		end
+		def control2X=(val)
+			self.controlPoint2 = [val, self.control2Y]
+		end
+		def control2Y=(val)
+			self.controlPoint2 = [self.control2X, val]
+		end
+		def endX=(val)
+			self.endPoint = [val, self.endY]
+		end
+		def endY=(val)
+			self.endPoint = [self.endX, val]
+		end
 		alias :at :[]
 		alias :inspect :to_s
 	end
@@ -114,5 +303,12 @@ module Kage
 				raise TypeError, "Not a Kage::Stroke instance"
 			end
 		end
+	end
+	
+	def self.dist(from, to)
+		for a in [from, to]
+			raise TypeError, "Not an Array of 2 Numeric instances" unless a.is_a? Array and a.length == 2 and a.all?{|elem| elem.is_a? Numeric}
+		end
+		Math.hypot(to[0] - from[0], to[1] - from[1])
 	end
 end
--- kage-socho.rb
+++ kage-socho.rb
@@ -16,13 +16,13 @@ while l = ARGF.gets
 				stat['walkRadical']['stat'] += 1
 			elsif stroke[0..2] == [1, 0, 2] and stroke.strokelength < 60 and [1, 2].include?(stat['walkRadical']['stat']) then
 				stat['walkRadical']['stat'] += 2
-				stat['walkRadical']['tmpPos'] = stroke[5..6]
-			elsif stroke[0..2] == [1, 22, 0] and [3, 4].include?(stat['walkRadical']['stat']) and stroke[3..4] == stat['walkRadical']['tmpPos'] then
+				stat['walkRadical']['tmpPos'] = stroke.endPoint
+			elsif stroke[0..2] == [1, 22, 0] and [3, 4].include?(stat['walkRadical']['stat']) and stroke.startPoint == stat['walkRadical']['tmpPos'] then
 				stat['walkRadical']['stat'] += 2
-				stat['walkRadical']['tmpPos'] = stroke[5..6]
-			elsif stroke[0..2] == [2, 0, 7] and [5, 6].include?(stat['walkRadical']['stat']) and Math.hypot(stroke[7] - stat['walkRadical']['tmpPos'][0], stroke[8] - stat['walkRadical']['tmpPos'][1]) < 12 then
+				stat['walkRadical']['tmpPos'] = stroke.endPoint
+			elsif stroke[0..2] == [2, 0, 7] and [5, 6].include?(stat['walkRadical']['stat']) and Kage.dist(stroke.endPoint, stat['walkRadical']['tmpPos']) < 12 then
 				stat['walkRadical']['stat'] += 2
-			elsif (stroke[0..2] == [2, 7, 0] or stroke[0..2] == [6, 7, 0]) and [7, 8].include?(stat['walkRadical']['stat']) and Math.hypot(stroke[3] - stat['walkRadical']['tmpPos'][0], stroke[4] - stat['walkRadical']['tmpPos'][1]) < 12 then
+			elsif (stroke[0..2] == [2, 7, 0] or stroke[0..2] == [6, 7, 0]) and [7, 8].include?(stat['walkRadical']['stat']) and Kage.dist(stroke.startPoint, stat['walkRadical']['tmpPos']) < 12 then
 				stat['walkRadical']['index'] = index - stat['walkRadical']['stat'] + 3
 				stat['walkRadical']['type'] = stat['walkRadical']['stat'] - 6
 				stat['walkRadical']['stat'] = 0
@@ -36,61 +36,61 @@ while l = ARGF.gets
 		# 特定部首を宋朝体字形に置換え
 		if not stat['walkRadical']['index'].nil? then # 之繞
 			index = stat['walkRadical']['index'] + stat['walkRadical']['type']
-			baseWidth = glyph[index].to_a[5] - glyph[index].to_a[3]
+			baseWidth = glyph[index].endX - glyph[index].startX
 			
-			stroke = glyph[index].to_a
-			stroke[3] += baseWidth * 2 / 9
-			stroke[4] += 4
-			stroke[5] += baseWidth * 2 / 9
+			stroke = glyph[index]
+			stroke.startX += baseWidth * 2 / 9
+			stroke.startY += 4
+			stroke.endX += baseWidth * 2 / 9
 			glyph[index] = stroke
 			
-			stroke = glyph[index + 1].to_a
-			stroke[0] = 2
-			stroke[2] = 7
-			stroke.push(stroke[5], stroke[6])
-			stroke[3] += baseWidth * 2 / 9
-			stroke[5] = (stroke[3] * 2 + stroke[7]) / 3
-			stroke[6] = (stroke[4] + stroke[8]) / 2
-			stroke[7] -= baseWidth / 9
-			stroke[8] -= 1
+			stroke = glyph[index + 1]
+			stroke.strokeType = 2
+			stroke.endType = 7
+			stroke.startX += baseWidth * 2 / 9
+			stroke.control1X = (stroke.startX * 2 + stroke.endX) / 3
+			stroke.control1Y = (stroke.endY + stroke.endY) / 2
+			stroke.endX -= baseWidth / 9
+			stroke.endY -= 1
 			glyph[index + 1] = stroke
 			
-			stroke = glyph[index + 2].to_a
-			stroke[4] = (stroke[4] - stroke[8]) / 2 + stroke[8]
-			stroke[6] = (stroke[6] - stroke[8]) / 2 + stroke[8]
-			stroke[7] -= baseWidth * 2 / 9
+			stroke = glyph[index + 2]
+			stroke.startY = (stroke.startY - stroke.endY) / 2 + stroke.endY
+			stroke.control1Y = (stroke.control1Y - stroke.endY) / 2 + stroke.endY
+			stroke.endX -= baseWidth * 2 / 9
 			glyph[index + 2] = stroke
 			
-			stroke = glyph[index + 3].to_a
-			if stroke[0] == 6 then
-				stroke[0] = 2
-				stroke[7..10] = stroke[9..10]
+			stroke = glyph[index + 3]
+			if stroke.strokeType == 6 then
+				stroke.endY = stroke.control2Y
+				stroke.controlPoint2 = stroke.controlPoint1
+				stroke.strokeType = 2
 			end
-			stroke[3] -= baseWidth * 2 / 9
-			if stroke[6] > stroke[8] then
-				stroke[6], stroke[8] = stroke[8], stroke[6]
+			stroke.startX -= baseWidth * 2 / 9
+			if stroke.control1Y > stroke.endY then
+				stroke.control1Y, stroke.endY = stroke.endY, stroke.control1Y
 			end
 			glyph[index + 3] = stroke
 			
 			if stat['walkRadical']['type'] == 1 then
-				baseY = glyph[index - 1].to_a[8]
-				stroke0 = glyph[index + 0].to_a
-				stroke1 = glyph[index + 1].to_a
-				spanY = (stroke1[4] - baseY) / 2
-				stroke0[4] -= spanY / 2
-				stroke0[6] -= spanY / 2
-				stroke1[4] -= spanY / 2
-				downHeight = stroke1[8] - stroke1[4] - 1
-				stroke1[6] = (stroke1[6] - stroke1[4]) * 2 / 5 + stroke1[4]
-				stroke1[8] = (stroke1[8] - stroke1[4]) * 2 / 5 + stroke1[4]
+				baseY = glyph[index - 1].endY
+				stroke0 = glyph[index + 0]
+				stroke1 = glyph[index + 1]
+				spanY = (stroke1.startY - baseY) / 2
+				stroke0.startY -= spanY / 2
+				stroke0.endY -= spanY / 2
+				stroke1.startY -= spanY / 2
+				downHeight = stroke1.endY - stroke1.startY - 1
+				stroke1.control1Y = (stroke1.control1Y - stroke1.startY) * 2 / 5 + stroke1.startY
+				stroke1.endY = (stroke1.endY - stroke1.startY) * 2 / 5 + stroke1.startY
 				stroke2 = [2, 7, 8,
-				           stroke1[7], stroke1[8] - 1,
-				           stroke1[3] + 1, stroke1[4] + downHeight * 55 / 100,
-				           stroke1[3], stroke1[4] + downHeight * 7 / 10 + 1]
+				           stroke1.endX, stroke1.endY - 1,
+				           stroke1.startX + 1, stroke1.startY + downHeight * 55 / 100,
+				           stroke1.startX, stroke1.startY + downHeight * 7 / 10 + 1]
 				stroke3 = [2, 32, 7,
-				           stroke1[3], stroke1[4] + downHeight * 7 / 10 - 1,
-				           stroke1[3] - 1, stroke1[4] + downHeight * 9 / 10,
-				           stroke1[7], stroke1[4] + downHeight]
+				           stroke1.startX, stroke1.startY + downHeight * 7 / 10 - 1,
+				           stroke1.startX - 1, stroke1.startY + downHeight * 9 / 10,
+				           stroke1.endX, stroke1.startY + downHeight]
 				glyph[index] = stroke0
 				glyph[index + 1] = stroke1
 				glyph.insert(index + 2, stroke2, stroke3)
